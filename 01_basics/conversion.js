ba808ff8031988b9a194ed14a2fa7c1d36f2c160
razayworks <razayworks@gmail.com>
2026-05-30 07:03:59 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,28 @@
+let score = "33aff";
+
+// console.log(typeof(score));
+let value = Number(score);
+// console.log(typeof(value));
+// console.log(value);
+
+
+let temp = null;
+let result = Number(temp);
+// console.log(typeof(result));
+// console.log(result);
+
+// "33" => 33
+// "334aafv" => NAN
+// "Geoge" => NAN
+// true => 1, false => 0
+// undefined => NAN
+// null => 0
+
+
+let name = 0;
+let boolConResult = Boolean(name);
+console.log(typeof(boolConResult));
+console.log(boolConResult);
+
+// 0=> false 1=> true
+// "string" => true,  "" => false
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,13 @@
+let name = "George";
+console.log(typeof name);
+let password = 3455;
+console.log(typeof password);
+
+let city = null;
+let email;
+console.log(typeof city);
+console.log(typeof email);
+
+/* 
+    datatype of null is object and datatype of undefined is undefined
+*/
