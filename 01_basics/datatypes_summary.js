86b6615e3fcebfd3d3b7509cb9ce11dd4778fd1d
razayworks <razayworks@gmail.com>
2026-06-01 18:43:53 +0000

    premitives

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -32,4 +32,25 @@ greet();
 console.log(typeof greet);
 
 
+
 // types of array, objects and null are objects and types of function is function
+
+// Stack (Premitives Data Types) and Heap(Non-Primitive Data Types)
+let value1 = "George";
+let value2 = value1;
+console.log(value2);
+value2 = "John";
+console.log(value1);
+console.log(value2);
+
+let obj1 = {
+    name: "Tom",
+    email: "tom@example.com",
+};
+
+let obj2 = obj1;
+obj2.name = "Jerry";
+console.log(obj1.name);
+console.log(obj2.name);
+
+// In static(Prmitivies Data Types) example number, string, boolean, null, undefined, symbol are stored in stack and in dynamic(Non-Primitive Data Types) example arrays, functions and objects are stored in heap. 
